c6beb779967870f9a9bec5c99f5c988b3b44bec5
unknown <Administrator@.(none)>
2011-09-03 16:47:15 +0200

    mandatory password

--- main.py
+++ main.py
@@ -56,6 +56,7 @@ def main():
 	
 	# to change the opts below online from the webserver,
 	#   enable a secret admin url and access it like: /my_s3cr3t_adm1n?attack=focus
+	#   or None to disable
 	remote_admin = "rad_27d4"  # (no leading '/')
 
 	# all opts in one dict, so we can show them on http
