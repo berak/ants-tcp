b0b3d1a79b57848356515f92998e498200ec02d4
berak <p4p4p4@web.de>
2011-11-18 10:12:36 +0100

    updated ants.py from challenge (minor ANT_HILLS fix)

--- engine.py
+++ engine.py
@@ -421,7 +421,6 @@ def get_moves(game, bots, bot_nums, time_limit, turn):
                 # bot finished sending data for this turn
                 break
             bot_moves[b].append(line)
-            lines_read += 1
             line = bot.read_line()
 
         line = bot.read_error()
